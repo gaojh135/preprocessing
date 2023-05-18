06fd0a9d35b4c67ca664520a6deb08aac6b0db92
gaojh135 <gaojh135@gmail.com>
2023-05-18 11:20:52 +0800

    the first commit

---
+++ scripts/register_mri_to_ct.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Run this in a OS with unix-based syntax
+
+# Measure Elapsed time of the script:
+START_TIME=$SECONDS
+
+# Modify according to the location of your data
+initial='../Task1/brain/'
+dirOut='../Task1_resample/brain/'
+
+#Prepare calculation of elapsed time for the script and logging
+Now=`date`
+script_name=$(basename $0)
+echo -e $script_name $Now
+echo -e $script_name $Now  >> ${dirOut}Logfile.txt
+
+
+# Loop over patients and resample CT to 1x1x1
+declare -a patients
+readarray patients < ../txt/1_brain_train.txt
+readarray -t patients < ../txt/1_brain_train.txt
+
+for patIndex in $(seq 0 $((${#patients[*]}-1)))
+do
+    patient=`echo ${patients[patIndex]} | awk '{print $1}'`
+    
+    # Define paths to input and output data
+    patientnr=${patient:1}
+
+    # Define path to temporary output directory
+    TMP=${dirOut}${patient}
+    mkdir -p $TMP
+
+    # Resample CT to 1x1x1
+    echo "Registering..."
+    python ../pre_process_tools.py register --f ${TMP}ct_resampled.nii.gz --m ${TMP}mr_or.nii.gz --o ${TMP}mr_T1_registered.nii.gz --p ./param_files/parameters_MR.txt
+done
+
+ELAPSED_TIME=$(($SECONDS - $START_TIME))
+echo 'Time elapsed to prepare the data: ' $(($ELAPSED_TIME/60)) ' min' $(($ELAPSED_TIME%60)) ' s'
+echo 'Time elapsed to prepare the data: ' $(($ELAPSED_TIME/60)) ' min' $(($ELAPSED_TIME%60)) ' s' >> ${dirOut}Logfile.txt
---
+++ txt/xlsx-to-txt.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# Define path to input and output files
+input_file = '1_pelvis_train.xlsx'
+output_file = '1_pelvis_train.txt'
+
+# Define column names to read from input file
+columns_to_read = ['ID']
+
+# Read input file using pandas
+df = pd.read_excel(input_file, usecols=columns_to_read)
+
+# Write selected columns to output file
+with open(output_file, 'w') as f:
+    for index, row in df.iterrows():
+        f.write(str(row['ID']) + '\n')
+
